58b8cf3ffd90865194df7653e1d529ae8819f37c
on-stu <minsu0523@naver.com>
2022-07-20 16:08:04 +0900

    what is defer

--- main.go
+++ main.go
@@ -5,26 +5,14 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func lenAndUpperNaked(name string) (length int, uppercase string) { //naked function이라고 하는건데 return을 이 줄에서 바로 해줌
-	length = 1
-	uppercase = "HI"
+func lenAndUpper(name string) (length int, upperCase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	upperCase = strings.ToUpper(name)
 	return
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
 }
 
 func main() {
 	totalLength, upperName := lenAndUpper("nico")
 	fmt.Println(totalLength, upperName)
-	repeatMe("hi", "hello", "wow")
 }
