c9925e06c0d24e3d8ef17f385872fc10f05bc074
on-stu <minsu0523@naver.com>
2022-07-22 09:57:10 +0900

    struct and done principles

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	minsu := map[string]string{"name": "minsu", "age": "12"}
-	fmt.Println(minsu)
-	for key, value := range minsu {
-		fmt.Println(key, value)
-	}
+type person struct {
+	name string
+	age  int
 }
 
-// map은 약간 object나 dict와 비슷한건데 정확히는 다름.
-// 왜냐하면 모든 value의 타입이 같아야함
+func main() {
+	minsu := person{name: "minsu", age: 18}
+	fmt.Println(minsu.age)
+}
