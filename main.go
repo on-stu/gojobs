95ca62b2b75d25f56616cd49adb2a422393e7062
on-stu <minsu0523@naver.com>
2022-07-22 09:49:39 +0900

    maps

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"nico", "lynn", "dal"} //여기 길이를 지정해주면 array, 아니면 slice가 된다.
-	names = append(names, "wow")
-	fmt.Println(names)
+	minsu := map[string]string{"name": "minsu", "age": "12"}
+	fmt.Println(minsu)
+	for key, value := range minsu {
+		fmt.Println(key, value)
+	}
 }
-
-//&을 입력하면 메모리 주소를 줌
