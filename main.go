b0d97611ab30cc4212256a0701436a8f36eb4002
on-stu <minsu0523@naver.com>
2022-07-22 09:45:08 +0900

    arrays and slices

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	a = 200
-	fmt.Println(a, *b)
+	names := []string{"nico", "lynn", "dal"} //여기 길이를 지정해주면 array, 아니면 slice가 된다.
+	names = append(names, "wow")
+	fmt.Println(names)
 }
 
 //&을 입력하면 메모리 주소를 줌
