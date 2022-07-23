f0b7c4d5cf6d7b079bde0ddd1e759e141dc0fd72
on-stu <minsu0523@naver.com>
2022-07-23 17:05:27 +0900

    slow url checker done

--- go.mod
+++ go.mod
@@ -1,11 +1,3 @@
 module gojobs
 
 go 1.18
-
-require (
-	github.com/on-stu/gojobs/accounts v0.0.0
-)
-
-replace (
-	github.com/on-stu/gojobs/accounts v0.0.0 => ./accounts
-)
--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/on-stu/gojobs/accounts"
+	"net/http"
 )
 
-func main() {
-	account := accounts.NewAccount("minsu")
-	fmt.Println(account.Balance())
-	account.Deposit(10000)
-	fmt.Println(account.Balance())
+var errRequestFailed = errors.New("request failed")
 
-	fmt.Println(account)
+func main() {
+	var results = map[string]string{}
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking", url)
+	resp, err := http.Get(url)
+
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
